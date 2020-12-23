7b463731a97dab6b71601da79803072a7da95184
anevsevra <aliaksandr.neuseura@coverwallet.com>
2020-12-23 23:56:01 +0300

    Add seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,37 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     ElixirAssesment.Repo.insert!(%ElixirAssesment.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias ElixirAssesment.Repo
+alias ElixirAssesment.Datasets.Category
+alias ElixirAssesment.Datasets.Post
+
+categories = [
+  %{
+    description: "Category that does not require moderation",
+    keywords: ~w[apple tree fruit],
+    name: "gardening",
+    need_moderation: false,
+    tag: "gardening"
+  },
+  %{
+    description: "Another category that does not require moderation",
+    keywords: ~w[plane helicopter superman],
+    name: "aviation",
+    need_moderation: false,
+    tag: "aviation"
+  },
+  %{
+    description: "Category that requires moderation",
+    keywords: ~w[nasty evil],
+    name: "nasty_category",
+    need_moderation: true,
+    tag: "nasty"
+  }
+]
+
+posts = [
+  %{
+    title: "About Elixir",
+    text: "Nice PL!"
+  }
+]
+
+Enum.each(categories, fn attrs -> Category.changeset(%Category{}, attrs) |> Repo.insert! end)
+Enum.each(posts, fn attrs -> Post.changeset(%Post{}, attrs) |> Repo.insert! end)
